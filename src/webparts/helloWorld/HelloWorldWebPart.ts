5888a5434800ce0ac1425d673f88cbb9c956395f
O'Brien <Tristian.O'Brien@tpr.gov.uk>
2016-09-28 14:37:45 +0100

    end of tutorial 2

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -73,24 +73,24 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   public render(): void {
    this.domElement.innerHTML = `
       <div class="${styles.helloWorld}">
-      <div class="${styles.container}">
-          <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
-              <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
-                  <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
-                  <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
-                  <p class="ms-font-l ms-fontColor-white">${this.properties.description}</p>
-                  <p class="ms-font-l ms-fontColor-white">${this.properties.test2}</p>
-                  <p class='ms-font-l ms-fontColor-white'>Loading from ${this.context.pageContext.web.title}</p>
-                  <a href="https://github.com/SharePoint/sp-dev-docs/wiki" class="ms-Button ${styles.button}">
-                      <span class="ms-Button-label">Learn more</span>
-                  </a>
-              </div>
-          </div>
-      <div id="spListContainer" />
-      </div>
-  </div>`;
+        <div class="${styles.container}">
+            <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
+                <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+                    <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+                    <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
+                    <p class="ms-font-l ms-fontColor-white">${this.properties.description}</p>
+                    <p class="ms-font-l ms-fontColor-white">${this.properties.test2}</p>
+                    <p class='ms-font-l ms-fontColor-white'>Loading from ${this.context.pageContext.web.title}</p>
+                    <a href="https://github.com/SharePoint/sp-dev-docs/wiki" class="ms-Button ${styles.button}">
+                        <span class="ms-Button-label">Learn more</span>
+                    </a>
+                </div>
+            </div>
+            <div id="spListContainer" />
+        </div>
+    </div>`;
 
-  this._renderListAsync();
+    this._renderListAsync();
   }
 
   protected get propertyPaneSettings(): IPropertyPaneSettings {
